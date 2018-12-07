54621dc31cac3f146b3c016b8a7953ff3e2da606
Dalhome3 <da.ledger01@gmail.com>
2018-12-07 11:31:32 +0000

    Autocommit

---
+++ AutoQuestionGenerator/AutoQuestionGenerator/Controllers/GroupsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoQuestionGenerator.DatabaseModels;
+using AutoQuestionGenerator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoQuestionGenerator.Controllers
+{
+    public class GroupsController : Controller
+    {
+        IdentityModels _context;
+
+        public GroupsController()
+        {
+            _context = new IdentityModels();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create(bool closeAfter)
+        {
+            var model = new CreateGroupViewModel();
+            model.AccessTypes = _context.accessTypes.ToArray();
+            model.Users = _context.users.ToArray();
+            model.CloseAfter = closeAfter;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateGroupViewModel model)
+        {
+            return RedirectToAction("Close", "Home");
+        }
+    }
+}
---
+++ AutoQuestionGenerator/AutoQuestionGenerator/Models/CreateGroupViewModel.cs
@@ -0,0 +1,20 @@
+﻿using AutoQuestionGenerator.DatabaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoQuestionGenerator.Models
+{
+    public class CreateGroupViewModel
+    {
+        public string GroupName { get; set; }
+        public int AccessType { get; set; }
+        public int[] GroupUsers { get; set; }
+
+        public bool CloseAfter { get; set; }
+
+        public Users[] Users { get; set; }
+        public AccessTypes[] AccessTypes { get; set; }
+    }
+}
