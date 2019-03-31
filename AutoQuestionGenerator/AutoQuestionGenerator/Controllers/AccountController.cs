8d683c9a92dca02702f782cad393e5e10f5d2e38
Daniel Ledger <da.ledger01@gmail.com>
2019-03-31 12:44:56 +0100

    More comments

--- AutoQuestionGenerator/AutoQuestionGenerator/Controllers/AccountController.cs
+++ AutoQuestionGenerator/AutoQuestionGenerator/Controllers/AccountController.cs
@@ -15,26 +15,42 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace AutoQuestionGenerator.Controllers
 {
+    /// <summary>
+    /// This controller holds the methods for updating
+    /// viewing and controlling the infomration of the user.
+    /// </summary>
+    [Authorized]
+    [AutoValidateAntiforgeryToken]
     public class AccountController : Controller
     {
+        // Setup class wide variables for the program.
         IHubContext<FileHub> HubContext;
         PythonInterpreter pythonInterpreter;
         private IHostingEnvironment _env;
         public AccountController(IHubContext<FileHub> hubcontext, IHostingEnvironment env)
         {
+            // Use the variables that are passed into the controllers to setup my information.
             HubContext = hubcontext;
             _env = env;
         }
+
+        /// <summary>
+        /// Gets the information about the logged in user and returns
+        /// it to them for them to view.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             return View(UserHelper.GetUser(UserHelper.GetUserId(HttpContext.Session)));
         }
 
-        public IActionResult AddQuestions()
-        {
-            return View();
-        }
-
+        /// <summary>
+        /// If the user wants to update details about their accounts
+        /// we need them to validate that they are actully the user
+        /// before they can update their details.
+        /// </summary>
+        /// <param name="user">The User information from the page</param>
+        /// <returns>Page to revalidate the user</returns>
         [HttpPost]
         public IActionResult Index(Users user)
         {
@@ -47,12 +63,24 @@ namespace AutoQuestionGenerator.Controllers
             return View("Revalidate", model);
         }
 
+        /// <summary>
+        /// After the user has attempted to revalidate
+        /// we need to check it they are who they say
+        /// that they are. If they are not we log them out
+        /// and if they are send them tto the update page.
+        /// </summary>
+        /// <param name="model">The revalidation information</param>
+        /// <returns>A page or redirect depending on the password</returns>
         [HttpPost]
         public IActionResult Revalidate(RevalidateModel model)
         {
             var user = UserHelper.GetUser(model.id);
+
+            // Uses the hasher to compare the user's password and the
+            // password that they just entered.
             if (Hasher.ValidatePassword(model.Password, user.Password))
             {
+                // Let them update their details if they got it right.
                 UpdateUser ret = new UpdateUser()
                 {
                     UserID = user.UserID,
@@ -62,40 +90,71 @@ namespace AutoQuestionGenerator.Controllers
                 };
                 return View("Update", ret);
             }
+            // If the user got the password wrong
+            // they probably aren't the user
+            // so log them out.
             UserHelper.LogOut(HttpContext.Session);
             OrganisationHelper.LogOut(HttpContext.Session);
             return RedirectToAction("Index", "Home");
         }
 
+
+        /// <summary>
+        /// Updates the users accounts details if they
+        /// have changed any of them.
+        /// It also allows them to
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public IActionResult Update(UpdateUser model)
         {
+            // Check if the user has enetered correct details
             if (!ModelState.IsValid)
                 return View(model);
+
+            // Check that the user hasn't passed a XSS attack
+            // or tried to update form data.
             if (UserHelper.GetUserId(HttpContext.Session) == model.UserID)
             {
                 var user = UserHelper.GetUser(model.UserID);
 
+                // Checks if the user has updated their password
                 if (!string.IsNullOrWhiteSpace(model.Password) && !string.IsNullOrWhiteSpace(model.VerifyPassword))
                 {
+                    // If the password and verification are not the same
+                    // return the model with an error.
                     if(model.Password != model.VerifyPassword)
                     {
                         return View(model);
                     }
+                    // if they are update the users password.
                     user.Password = Hasher.Hash(model.Password);
                 }
+                // Update their names to things that have changed.
                 user.First_Name = model.Firstname;
                 user.Last_Name = model.Lastname;
                 user.Username = model.Username;
 
+                // Update the database
                 DatabaseConnector.Update(user);
 
                 return View("Index", user);
             }
+            // If their is an XSS attempt log the user out.
             UserHelper.LogOut(HttpContext.Session);
             OrganisationHelper.LogOut(HttpContext.Session);
             return RedirectToAction("Index", "Home");
+        }
+
+        
+        /// <summary>
+        /// Add the ability to upload new questions to the database if you are an Admin
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult AddQuestions()
+        {
+            return View();
         }
 
         [HttpPost]
