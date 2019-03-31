ce959dee04344a108c82350836a81ab67fd2eae3
dalhome3 <da.ledger01@gmail.com>
2019-03-31 14:08:28 +0100

    Autocommit

--- comit.bat
+++ comit.bat
@@ -1,9 +1,12 @@
 echo.
 echo This will commit my git every 5 Minutes... 
+F:
 cd "F:\CS Coursework\AutoQuestion-Generator-CW"
 :timer
   echo.
-  gc.bat
+  git add -A
+  git commit -m "Autocommit"
+  git push
   echo.
   TIMEOUT /T 300 /NOBREAK
 GOTO timer
